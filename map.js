3c4ebe771c49bf9da96544d2d510dc7ec24227d9
Daniel Potts <djpotts2891@gmail.com>
2023-05-23 21:09:47 +0000

    JS Map lesson complete

--- map.js
+++ map.js
@@ -6,15 +6,29 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = []
+for (let num of nums){
+  results.push (num * 2)
+}
+console.log(results)
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2
+}
+const mapResults = nums.map(multByTwo)
+console.log(mapResults)
 
 // Simplified w/ map()
 
+const Simplfied = nums.map(function (num) { return num * 2})
+console.log(Simplfied)
 
 // Simplfied w/ map() + arrow function
 
+const simpleArrow = nums.map(num => num * 2)
+console.log(simpleArrow)
 
 // With objects:
 const students = [
@@ -37,3 +51,5 @@ const students = [
     skill: 'CSS'
   },
 ];
+const studentsWithIds = students.map(student => [student.name, student.id])
+console.log(studentsWithIds)
